bd1bf0a306add945ba75c387a922eb737ce2f7a3
AlenaOcheretnaia <alenaocheretnaia@gmail.com>
2021-08-19 19:44:47 -0400

    Comment added in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,16 @@
+//PriorityBlockingQueue  -  не подходит, так как не гарантируется  FIFO, а для call центра этот принцип имеет значение
+//DelayQueue  - блокировка потоков, можно использовать, если реализовывать очередь с минимальным временем ожидания,
+// но не гарантируется FIFO + interface
+//ConcurrentLinkedDeque - работа с двух сторон, медленная - для нашей задачи избыточна
+//ArrayBlockingQueue - блокировка потоков, может работать по FIFO, нерасширяемая - не подходит для колл-центра,
+// либо должна быть заведомо очень большая
+// LinkedBlockingDeque - двунаправленная - избыточна для нашей задачи
+//SynchronousQueue - блокировка, пока не заберут, честная блокировка
+//LinkedTransferQueue - блокировка, гибкость - по таймауту
+//
+//  Выбирала между  LinkedBlockingQueue  и СoncurrentLinkedQueue - склонялась изначально к первой,
+//  но потом решила остановиться на очереди без блокировки потоков.
+
 import java.util.concurrent.ConcurrentLinkedQueue;
 import static java.lang.Thread.sleep;
 
